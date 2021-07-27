8cca05d34aa84862895f195edab4c9e9e9903ac4
Jeff Hykin <jeff.hykin@gmail.com>
2021-07-27 11:05:28 -0500

    upgrade the python setup

--- settings/project.config.sh
+++ settings/project.config.sh
@@ -9,3 +9,16 @@
 export PROJECTR_FOLDER="$PWD"
 export PROJECTR_HOME="$PROJECTR_FOLDER/settings/home/"
 export PROJECTR_COMMANDS_FOLDER="$PROJECTR_FOLDER/commands/"
+
+# 
+# run all the setups
+# 
+# this loop is so stupidly complicated because of many inherent-to-shell reasons, for example: https://stackoverflow.com/questions/13726764/while-loop-subshell-dilemma-in-bash
+for_each_item_in="$PROJECTR_FOLDER/settings/"; [ -z "$__NESTED_WHILE_COUNTER" ] && __NESTED_WHILE_COUNTER=0;__NESTED_WHILE_COUNTER="$((__NESTED_WHILE_COUNTER + 1))"; trap 'rm -rf "$__temp_var__temp_folder"' EXIT; __temp_var__temp_folder="$(mktemp -d)"; mkfifo "$__temp_var__temp_folder/pipe_for_while_$__NESTED_WHILE_COUNTER"; (find "$for_each_item_in" -maxdepth 1 ! -path . -print0 2>/dev/null | sort -z > "$__temp_var__temp_folder/pipe_for_while_$__NESTED_WHILE_COUNTER" &); while read -d $'\0' each
+do
+    # check if file exists
+    if [ -f "$each/#initialize.sh" ]
+    then
+        source "$each/#initialize.sh"
+    fi
+done < "$__temp_var__temp_folder/pipe_for_while_$__NESTED_WHILE_COUNTER";__NESTED_WHILE_COUNTER="$((__NESTED_WHILE_COUNTER - 1))"
--- settings/setup_automatically/steps/01900_setup_python_venv.sh
+++ settings/setup_automatically/steps/01900_setup_python_venv.sh
@@ -3,11 +3,8 @@ export TMPDIR="$HOME/tmp.cleanable"
 mkdir -p "$TMPDIR"
 export VIRTUAL_ENV="$PROJECTR_FOLDER/.venv"
 export PATH="$VIRTUAL_ENV/bin:$PATH"
-if ! [ -d "$VIRTUAL_ENV" ]
+if ! [[ -d "$VIRTUAL_ENV" ]]
 then
     echo "creating virtual env for python"
     python -m venv "$VIRTUAL_ENV" && echo "virtual env created"
 fi
-
-# fix SSL issues
-export SSL_CERT_FILE="$(python -c 'import ssl; print(ssl.get_default_verify_paths().openssl_cafile)')"  
