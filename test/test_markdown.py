9904ef089fd676754a1a07a373bfe0fef2ba7bb4
jeff.hykin <jeff.hykin@gmail.com>
2021-12-10 16:35:45 -0600

    fix some (not all) escaping problems

---
+++ test/test_markdown.py
@@ -0,0 +1,4 @@
+import silver_spectacle as ss
+markdown = """## What is this?Silver Spectacle is a python library for displaying data. Its designed as a (superior)
+ alternative to **matplotlib**."""
+ss.DisplayCard("quickMarkdown", markdown)
