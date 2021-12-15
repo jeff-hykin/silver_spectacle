10f1d3748fcfa7c3b6a2e2883f34305699d57292
jeff.hykin <jeff.hykin@gmail.com>
2021-12-15 11:53:29 -0600

    -

--- main/setup.py
+++ main/setup.py
@@ -5,7 +5,7 @@ with open("../README.md", "r") as file_handle:
  
 setuptools.setup(
     name='silver_spectacle',
-    version='0.4.1',
+    version='0.5.0',
     description="An easier way to display data",
     url='https://github.com/jeff-hykin/silver_spectacle',
     author='Jeff Hykin',
