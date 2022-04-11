4c3f93c4b954cba7faff3d894dff959fcd117c54
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-11 09:35:23 -0500

    add multiLine send example

--- test/test_multi_line.py
+++ test/test_multi_line.py
@@ -1,14 +1,30 @@
 import silver_spectacle as ss
 
-ss.DisplayCard("multiLine", dict(
+card = ss.DisplayCard("multiLine", dict(
     # name : [ [x1,y1], [x2,y2],  ]
-    line1=[
+    Line1Name=[
         [ 1,   2   ],
         [ 1.5, 2.3 ],
         [ 2,   3   ],
         [ 5,   5   ],
     ],
-    line2=[
+    Line2Name=[
+        [ 1+1,   2   ],
+        [ 1+1.5, 2.3 ],
+        [ 1+2,   3   ],
+        [ 1+5,   5   ],
+    ],
+))
+
+# Live update
+card.send(dict(
+    Line1Name=[
+        [ 1,   2   ],
+        [ 1.5, 2.3 ],
+        [ 2,   3   ],
+        [ 5,   5   ],
+    ],
+    Line2Name=[
         [ 1+1,   2   ],
         [ 1+1.5, 2.3 ],
         [ 1+2,   3   ],
